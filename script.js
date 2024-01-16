29ff0260aaaf81e509e4fee400a5e3f2fb259caf
Ayesha20031 <77922652+Ayesha20031@users.noreply.github.com>
2024-01-16 20:52:25 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let menubtn =document.querySelector('#menu_bars');
-let mynav =document.querySelector('.navbar');
-
-menubtn.onclick = () =>{
-    menubtn.classList.toggle('fa-times');
-    mynav.classList.toggle('active');
-}
